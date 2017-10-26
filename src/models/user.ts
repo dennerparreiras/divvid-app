8e863baa83e429520fa30e9e0de4f38f37fbc462
Denner <rennedenner@icloud.com>
2017-10-25 22:03:58 -0200

    Models created

---
+++ src/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  
+    constructor(fields: any) {
+      // Quick and dirty extend/assign fields to this model
+      for (const f in fields) {
+        this[f] = fields[f];
+      }
+    }
+  
+  }

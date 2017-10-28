f187a01a2ffb7ce9138639e89e98d4342feb3d0e
Denner Parreiras <rennedenner@icloud.com>
2017-10-27 23:28:44 -0200

    Exclusão de contas (bills)

--- src/pages/bill-detail/bill-detail.ts
+++ src/pages/bill-detail/bill-detail.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, Platform  } from 'ionic-angular';
 
 import { Bills } from '../../providers/providers';
 
@@ -10,9 +10,44 @@ import { Bills } from '../../providers/providers';
 })
 export class BillDetailPage {
   bill: any;
+  bills: any;
+  actionsheetCtrl: any;
+  platform: any;
 
-  constructor(public navCtrl: NavController, navParams: NavParams, bills: Bills) {
+  constructor(
+    public navCtrl: NavController, navParams: NavParams, bills: Bills, platform: Platform, actionSheetCtrl: ActionSheetController) {
     this.bill = navParams.get('bill') || bills.defaultBill;
+    this.bills = bills;
+    this.actionsheetCtrl = actionSheetCtrl;
+    this.platform = platform;
   }
 
+  /**
+   * Delete the bill.
+   */
+  deleteBill(bill) {
+    this.bills.delete(bill);
+  }  
+
+  /**
+ * Open a menu to confirm to Delete an bill from the list of bills.
+ */
+  openMenu(bill) {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Você tem certeza que quer deletar?',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Deletar',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            this.deleteBill(bill);
+            this.navCtrl.popToRoot();
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 }
